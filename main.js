7cc30d11c69d77502b6d1fc095ffcd4f312874e9
722sofiaameh <sofiagabriel722@gmail.com>
2022-11-01 20:28:11 -0700

    add updates

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
-document.querySelector("#button").addEventListener("click", loadAdvice);
+// Using AJAX Method..
+// document.querySelector("#button").addEventListener("click", loadAdvice);
+// document.addEventListener("DOMContentLoaded", loadAdvice);
 
-function loadAdvice() {
-  const xhr = new XMLHttpRequest();
+// function loadAdvice() {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open("GET", "	https://api.adviceslip.com/advice", true);
+//   xhr.onload = function () {
+//     if (this.status === 200) {
+//       const advice = JSON.parse(this.responseText);
+//       //  console.log(this.response);
+//       const adviceText = document.getElementById('output');
+//       adviceText.innerText = JSON.stringify(advice.slip.advice);
+//       const adviceId = document.getElementById('advice-Id');
+//       adviceId.innerText = JSON.stringify (advice.slip.id);
+    
+//     }
+//   };
 
-  xhr.open("GET", "	https://api.adviceslip.com/advice", true);
+//   xhr.send();
+// }
+//END...
 
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const advice = JSON.parse(this.responseText);
-      //  console.log(this.response);
-      const adviceText = document.getElementById('output');
-      adviceText.innerText = JSON.stringify(advice.slip.advice);
-      const adviceId = document.getElementById('advice-Id');
-      adviceId.innerText = JSON.stringify (advice.slip.id);
-      // document.querySelector("#output").innerHTML = output;
-    }
-  };
 
-  xhr.send();
-}
- document.addEventListener("DOMContentLoaded",loadAdvice);
+// Using Fetch/Promises Method...
+// document.querySelector("#button").addEventListener('click', loadAdvice);
+// document.addEventListener("DOMContentLoaded", loadAdvice);
+
+// function loadAdvice () { fetch("https://api.adviceslip.com/advice")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((advice) => {
+//     const adviceText = document.getElementById("output");
+//     adviceText.innerText = JSON.stringify(advice.slip.advice);
+//     const adviceId = document.getElementById('advice-Id');
+//     adviceId.innerText = JSON.stringify (advice.slip.id);
+//   })
+//   .catch(err =>{
+//     console.log(err)
+//   })
+// };
+
+ //END 
+
+ //using async/await method..
+ document.querySelector("#button").addEventListener("click", loadAdvice);
+ document.addEventListener("DOMContentLoaded", loadAdvice);
+ async function loadAdvice (){
+  const response = await fetch ('https://api.adviceslip.com/advice')
+
+  const data = await response.json();
+  return data;
+ }
+ loadAdvice().then(advice=>{console.log(advice.slip)
+  const adviceText = document.getElementById("output");
+  adviceText.innerText = JSON.stringify(advice.slip.advice);
+  const adviceId = document.getElementById('advice-Id');
+  adviceId.innerText = JSON.stringify (advice.slip.id);
+})

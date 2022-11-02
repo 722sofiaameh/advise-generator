76417db475a85ce83802e760235ffe3642813253
722sofiaameh <sofiagabriel722@gmail.com>
2022-11-01 20:31:38 -0700

    add updates

--- main.js
+++ main.js
@@ -23,38 +23,38 @@
 
 
 // Using Fetch/Promises Method...
-// document.querySelector("#button").addEventListener('click', loadAdvice);
-// document.addEventListener("DOMContentLoaded", loadAdvice);
+document.querySelector("#button").addEventListener('click', loadAdvice);
+document.addEventListener("DOMContentLoaded", loadAdvice);
 
-// function loadAdvice () { fetch("https://api.adviceslip.com/advice")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((advice) => {
-//     const adviceText = document.getElementById("output");
-//     adviceText.innerText = JSON.stringify(advice.slip.advice);
-//     const adviceId = document.getElementById('advice-Id');
-//     adviceId.innerText = JSON.stringify (advice.slip.id);
-//   })
-//   .catch(err =>{
-//     console.log(err)
-//   })
-// };
+function loadAdvice () { fetch("https://api.adviceslip.com/advice")
+  .then((response) => {
+    return response.json();
+  })
+  .then((advice) => {
+    const adviceText = document.getElementById("output");
+    adviceText.innerText = JSON.stringify(advice.slip.advice);
+    const adviceId = document.getElementById('advice-Id');
+    adviceId.innerText = JSON.stringify (advice.slip.id);
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+};
 
  //END 
 
  //using async/await method..
- document.querySelector("#button").addEventListener("click", loadAdvice);
- document.addEventListener("DOMContentLoaded", loadAdvice);
- async function loadAdvice (){
-  const response = await fetch ('https://api.adviceslip.com/advice')
+//  document.querySelector("#button").addEventListener("click", loadAdvice);
+//  document.addEventListener("DOMContentLoaded", loadAdvice);
+//  async function loadAdvice (){
+//   const response = await fetch ('https://api.adviceslip.com/advice')
 
-  const data = await response.json();
-  return data;
- }
- loadAdvice().then(advice=>{console.log(advice.slip)
-  const adviceText = document.getElementById("output");
-  adviceText.innerText = JSON.stringify(advice.slip.advice);
-  const adviceId = document.getElementById('advice-Id');
-  adviceId.innerText = JSON.stringify (advice.slip.id);
-})
+//   const data = await response.json();
+//   return data;
+//  }
+//  loadAdvice().then(advice=>{console.log(advice.slip)
+//   const adviceText = document.getElementById("output");
+//   adviceText.innerText = JSON.stringify(advice.slip.advice);
+//   const adviceId = document.getElementById('advice-Id');
+//   adviceId.innerText = JSON.stringify (advice.slip.id);
+// })
